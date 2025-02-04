442cf6a4ee4f294b0fdca44c17ddf296beb6ef16
Evgeniy Bozko <drakon-rulit@mail.ru>
2025-02-04 22:54:59 +0300

    Внес изменения в домашнюю работу "Рога и копыта"

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.sql.SQLOutput;
-
 public class HomeWork  {
     public static void main(String[] args) {
         // Рога и Копыта
@@ -14,9 +12,9 @@ public class HomeWork  {
         int priceHam = 350;
         int priceNeck = 500;
 
-        int priceSausageMin = 414;
-        int priceSausageAver = 408;
-        int priceSausageMax = 404;
+        int costUpTo1000Kg = 414;
+        int costUpFrom1000To2000Kg = 408;
+        int costUpTo2000Kg = 404;
 
         int priceHamProd = 275;
         int priceNeckMin = 311;
@@ -24,43 +22,50 @@ public class HomeWork  {
 
         if (massSausage >= 1000);
         {
-            System.out.println("Себестоимость сосисок до 1000 кг");
-            System.out.println(massSausage * priceSausageMin);
+            int costSausageUpTo1000Kg = massSausage * costUpTo1000Kg;
+            System.out.println("Себестоимость сосисок до 1000 кг" + costSausageUpTo1000Kg);
 
         } if (massSausage < 2000);
         {
-            System.out.println("Себестоимость сосисок до 2000 кг");
-            System.out.println(massSausage * priceSausageAver);
+            int costSausageUpFrom1000To2000Kg = massSausage * costUpFrom1000To2000Kg;
+            System.out.println("Себестоимость сосисок до 2000 кг" + costSausageUpFrom1000To2000Kg);
+
         } if (massSausage > 2000);
-        {   System.out.println("Себестоимость сосисок больше 2000 кг");
-            System.out.println(massSausage * priceSausageMax);
-            System.out.println("Себестоимость ветчины");
-            System.out.println(massHam * priceHam);
-            System.out.println("Стоиость ветчины на производстве");
-            System.out.println(massHam * priceHamProd);
-            System.out.println("Себестоимость шейки");
-            System.out.println(massNeck * priceNeck);
-            System.out.println("Себестоимость шейки при производсте меньше 500 кг");
-            System.out.println(massNeck * priceNeckMin);
-            System.out.println("Себестоимость шейки при производстве 500 кг и больше");
-            System.out.println(massNeck * priceNeckMax);
+        {
+            int costSausageUpTo2000Kg = massSausage * costUpTo2000Kg;
+            System.out.println("Себестоимость сосисок больше 2000 кг" + costSausageUpTo2000Kg);
+
+            int costHam = massHam * priceHam;
+            System.out.println("Себестоимость ветчины" + costHam);
+            int costHamProd = massHam * priceHamProd;
+            System.out.println("Стоиость ветчины на производстве" + costHamProd);
+            int costNeck = massNeck * priceNeck;
+            System.out.println("Себестоимость шейки" + costNeck);
+            int costNeckUpTo500Kg = massNeck * priceNeckMin;
+            System.out.println("Себестоимость шейки при производсте меньше 500 кг" + costNeckUpTo500Kg);
+
+            int costNeckUpTo500KgToMore = massNeck * priceNeckMax;
+            System.out.println("Себестоимость шейки при производстве 500 кг и больше" + costNeckUpTo500KgToMore);
+
 
             //Чистая прибыль
 
-            System.out.println("Чистая прибыль сосисок");
-            System.out.println(massSausage * priceSausageAver + 100000000);
-            System.out.println("Чистая прибыль ветчины");
-            System.out.println(massHam * priceHamProd + 100000000);
-            System.out.println("Чистая прибыль шейки");
-            System.out.println(massNeck * priceNeckMax + 100000000);
+            int netProfitSausage = massSausage * costUpFrom1000To2000Kg + 100000000;
+            System.out.println("Чистая прибыль сосисок" + netProfitSausage);
+            int netProfitHam = massHam * priceHamProd + 100000000;
+            System.out.println("Чистая прибыль ветчины" + netProfitHam);
+            int netProfitNeck = massNeck * priceNeckMax + 100000000;
+            System.out.println("Чистая прибыль шейки" + netProfitNeck);
 
-            // Прибыль после налогов (вся прибыль до 2_000_000
+            // Прибыль после налогов (вся прибыль до 2_000_000)
 
-            System.out.println("Чистая прибыль после налогов с сосисок");
-            System.out.println((massSausage * priceSausageAver + 1000000) * 10 / 100);
-            System.out.println("Чистая прибыль с ветчины");
-            System.out.println((massHam * priceHamProd + 1000000) * 10 / 100);
-            System.out.println("Чистая прибыль с шейки");
-            System.out.println((massNeck * priceNeckMax + 1000000) * 10 / 100);
+            int netProfitSausageAfterTax = (massSausage * costUpFrom1000To2000Kg + 1000000) * 10 / 100;
+            System.out.println("Чистая прибыль после налогов с сосисок" + netProfitSausageAfterTax);
+            int netProfitHamAfterTax = ((massHam * priceHamProd + 1000000) * 10 / 100);
+            System.out.println("Чистая прибыль с ветчины" + netProfitHamAfterTax);
+            int netProfitNeckAfterTax = (massNeck * priceNeckMax + 1000000) * 10 / 100;
+            System.out.println("Чистая прибыль с шейки" + netProfitNeckAfterTax);
+
+            // The End.
 
     }}}
